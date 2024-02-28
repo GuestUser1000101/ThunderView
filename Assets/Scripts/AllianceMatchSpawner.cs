4240eabbc0bedd49e341b6c502abf93ec2c7b72f
GuestUser1000101 <ryanzou2008@gmail.com>
2024-02-27 18:19:12 -0800

    Partially changed subjective alliance box layout.

--- Assets/Scripts/AllianceMatchSpawner.cs
+++ Assets/Scripts/AllianceMatchSpawner.cs
@@ -10,6 +10,9 @@ public class AllianceMatchSpawner : MonoBehaviour
     public GameObject dataManagerObject;
     private DataManager dataManager;
     private string filepath;
+    private Color white = new Color(0.9764706f, 0.9607843f, 0.9490196f, 1f);
+    private Color yellow = new Color(0.9294118f, 0.7568628f, 0.5568628f, 1f);
+    private Color red = new Color(0.8901961f, 0.5882353f, 0.5333334f);
     // Start is called before the first frame update
     void Start()
     {
@@ -42,53 +45,57 @@ public class AllianceMatchSpawner : MonoBehaviour
                 if (!(matchJson.Team1.ToString() == selective || matchJson.Team2.ToString() == selective || matchJson.Team3.ToString() == selective)) {  continue; }
             }
             GameObject newMatchCell = matchPrefab;
-            newMatchCell.transform.GetChild(0).GetComponent<Text>().text = matchJson.MatchNumber.ToString();
-            newMatchCell.transform.GetChild(1).GetComponent<Text>().text = matchJson.MatchType;
-            newMatchCell.transform.GetChild(2).GetComponent<Text>().text = $"Data Quality: {matchJson.DataQuality.ToString()}/5";
-            newMatchCell.transform.GetChild(3).GetComponent<Text>().text = $"Scouter Name: {matchJson.ScouterName}";
-            newMatchCell.transform.GetChild(4).gameObject.SetActive(matchJson.Replay);
+            childOfCell(0).GetComponent<Text>().text = matchJson.MatchNumber.ToString();
+            childOfCell(1).GetComponent<Text>().text = matchJson.MatchType;
+            childOfCell(2).GetComponent<Text>().text = $"Data Quality: {matchJson.DataQuality.ToString()}/5";
+            childOfCell(3).GetComponent<Text>().text = $"Scouter Name: {matchJson.ScouterName}";
+            childOfCell(4).gameObject.SetActive(matchJson.Replay);
 
             // Team 1 Stats
-            newMatchCell.transform.GetChild(5).GetComponent<Text>().text = matchJson.Team1.ToString();
-            newMatchCell.transform.GetChild(5).GetChild(0).GetComponent<Text>().text = $"Avoidance Score {matchJson.Team1Avoid}";
-            newMatchCell.transform.GetChild(5).GetChild(0).GetComponent<Text>().color = matchJson.Team1Avoid == 3 ? Color.green : matchJson.Team1Avoid == 2 ? Color.white : Color.black;
-            newMatchCell.transform.GetChild(5).GetChild(1).GetComponent<Text>().text = $"Travel Speed Score {matchJson.Team1TravelSpeed}";
-            newMatchCell.transform.GetChild(5).GetChild(1).GetComponent<Text>().color = matchJson.Team1TravelSpeed == 3 ? Color.green : matchJson.Team1TravelSpeed == 2 ? Color.white : Color.black;
-            newMatchCell.transform.GetChild(5).GetChild(2).GetComponent<Text>().text = $"Align Speed Score {matchJson.Team1AlignSpeed}";
-            newMatchCell.transform.GetChild(5).GetChild(2).GetComponent<Text>().color = matchJson.Team1AlignSpeed == 3 ? Color.green : matchJson.Team1AlignSpeed == 2 ? Color.white : Color.black;
+            childOfCell(5).GetComponent<Text>().text = matchJson.Team1.ToString();
+            childOfCell(5).GetChild(0).GetComponent<BarDisplay>().setValue(matchJson.Team1Avoid);
+            childOfCell(5).GetChild(0).GetComponent<BarDisplay>().setColor(matchJson.Team1Avoid == 3 ? white : matchJson.Team1Avoid == 2 ? yellow : red);
+            childOfCell(5).GetChild(1).GetComponent<BarDisplay>().setValue(matchJson.Team1TravelSpeed);
+            childOfCell(5).GetChild(1).GetComponent<BarDisplay>().setColor(matchJson.Team1TravelSpeed == 3 ? white : matchJson.Team1TravelSpeed == 2 ? yellow : red);
+            childOfCell(5).GetChild(2).GetComponent<BarDisplay>().setValue(matchJson.Team1AlignSpeed);
+            childOfCell(5).GetChild(2).GetComponent<BarDisplay>().setColor(matchJson.Team1AlignSpeed == 3 ? white : matchJson.Team1AlignSpeed == 2 ? yellow : red);
 
             // Team 2 Stats
-            newMatchCell.transform.GetChild(6).GetComponent<Text>().text = matchJson.Team2.ToString();
-            newMatchCell.transform.GetChild(6).GetChild(0).GetComponent<Text>().text = $"Avoidance Score {matchJson.Team2Avoid}";
-            newMatchCell.transform.GetChild(6).GetChild(0).GetComponent<Text>().color = matchJson.Team2Avoid == 3 ? Color.green : matchJson.Team2Avoid == 2 ? Color.white : Color.black;
-            newMatchCell.transform.GetChild(6).GetChild(1).GetComponent<Text>().text = $"Travel Speed Score {matchJson.Team2TravelSpeed}";
-            newMatchCell.transform.GetChild(6).GetChild(1).GetComponent<Text>().color = matchJson.Team2TravelSpeed == 3 ? Color.green : matchJson.Team2TravelSpeed == 2 ? Color.white : Color.black;
-            newMatchCell.transform.GetChild(6).GetChild(2).GetComponent<Text>().text = $"Align Speed Score {matchJson.Team2AlignSpeed}";
-            newMatchCell.transform.GetChild(6).GetChild(2).GetComponent<Text>().color = matchJson.Team2AlignSpeed == 3 ? Color.green : matchJson.Team2AlignSpeed == 2 ? Color.white : Color.black;
+            childOfCell(6).GetComponent<Text>().text = matchJson.Team2.ToString();
+            childOfCell(6).GetChild(0).GetComponent<Text>().text = $"Avoidance Score {matchJson.Team2Avoid}";
+            childOfCell(6).GetChild(0).GetComponent<Text>().color = matchJson.Team2Avoid == 3 ? white : matchJson.Team2Avoid == 2 ? yellow : red;
+            childOfCell(6).GetChild(1).GetComponent<Text>().text = $"Travel Speed Score {matchJson.Team2TravelSpeed}";
+            childOfCell(6).GetChild(1).GetComponent<Text>().color = matchJson.Team2TravelSpeed == 3 ? white : matchJson.Team2TravelSpeed == 2 ? yellow : red;
+            childOfCell(6).GetChild(2).GetComponent<Text>().text = $"Align Speed Score {matchJson.Team2AlignSpeed}";
+            childOfCell(6).GetChild(2).GetComponent<Text>().color = matchJson.Team2AlignSpeed == 3 ? white : matchJson.Team2AlignSpeed == 2 ? yellow : red;
 
             // Team 3 Stats
-            newMatchCell.transform.GetChild(7).GetComponent<Text>().text = matchJson.Team3.ToString();
-            newMatchCell.transform.GetChild(7).GetChild(0).GetComponent<Text>().text = $"Avoidance Score {matchJson.Team3Avoid}";
-            newMatchCell.transform.GetChild(7).GetChild(0).GetComponent<Text>().color = matchJson.Team3Avoid == 3 ? Color.green : matchJson.Team3Avoid == 2 ? Color.white : Color.black;
-            newMatchCell.transform.GetChild(7).GetChild(1).GetComponent<Text>().text = $"Travel Speed Score {matchJson.Team3TravelSpeed}";
-            newMatchCell.transform.GetChild(7).GetChild(1).GetComponent<Text>().color = matchJson.Team3TravelSpeed == 3 ? Color.green : matchJson.Team3TravelSpeed == 2 ? Color.white : Color.black;
-            newMatchCell.transform.GetChild(7).GetChild(2).GetComponent<Text>().text = $"Align Speed Score {matchJson.Team3AlignSpeed}";
-            newMatchCell.transform.GetChild(7).GetChild(2).GetComponent<Text>().color = matchJson.Team3AlignSpeed == 3 ? Color.green : matchJson.Team3AlignSpeed == 2 ? Color.white : Color.black;
+            childOfCell(7).GetComponent<Text>().text = matchJson.Team3.ToString();
+            childOfCell(7).GetChild(0).GetComponent<Text>().text = $"Avoidance Score {matchJson.Team3Avoid}";
+            childOfCell(7).GetChild(0).GetComponent<Text>().color = matchJson.Team3Avoid == 3 ? white : matchJson.Team3Avoid == 2 ? yellow : red;
+            childOfCell(7).GetChild(1).GetComponent<Text>().text = $"Travel Speed Score {matchJson.Team3TravelSpeed}";
+            childOfCell(7).GetChild(1).GetComponent<Text>().color = matchJson.Team3TravelSpeed == 3 ? white : matchJson.Team3TravelSpeed == 2 ? yellow : red;
+            childOfCell(7).GetChild(2).GetComponent<Text>().text = $"Align Speed Score {matchJson.Team3AlignSpeed}";
+            childOfCell(7).GetChild(2).GetComponent<Text>().color = matchJson.Team3AlignSpeed == 3 ? white : matchJson.Team3AlignSpeed == 2 ? yellow : red;
 
-            newMatchCell.transform.GetChild(8).GetChild(0).GetComponent<Text>().text = $"Auto Center Notes: {matchJson.AutoCenterNotes}";
-            newMatchCell.transform.GetChild(8).GetChild(1).GetComponent<Text>().text = $"High Notes: {matchJson.HighNotes}/{matchJson.HighNotePotential}";
-            newMatchCell.transform.GetChild(8).GetChild(2).GetComponent<Text>().text = $"Amplify Count: {matchJson.AmplifyCount}";
-            newMatchCell.transform.GetChild(8).GetChild(3).GetComponent<Text>().text = $"Team At Amp: {matchJson.TeamAtAmp}";
-            newMatchCell.transform.GetChild(8).GetChild(4).GetComponent<Text>().text = $"Bots in Harmony: {matchJson.Harmony}";
-            newMatchCell.transform.GetChild(8).GetChild(5).gameObject.SetActive(matchJson.Coopertition);
-            newMatchCell.transform.GetChild(8).GetChild(6).GetComponent<Text>().text = $"Fouls: {matchJson.Fouls}";
-            newMatchCell.transform.GetChild(9).GetChild(0).GetChild(0).GetChild(0).GetChild(0).GetComponent<Text>().text = $"Ranking Explanation: {matchJson.RankingComments}\nGeneral Strategy: {matchJson.StratComments}\nOther Comments:{matchJson.OtherComments}";
-            newMatchCell.transform.GetChild(10).GetComponent<Text>().text = matchJson.WinMatch ? "WIN" : "LOSS";
+            childOfCell(8).GetChild(0).GetComponent<Text>().text = $"Auto Center Notes: {matchJson.AutoCenterNotes}";
+            childOfCell(8).GetChild(1).GetComponent<Text>().text = $"High Notes: {matchJson.HighNotes}/{matchJson.HighNotePotential}";
+            childOfCell(8).GetChild(2).GetComponent<Text>().text = $"Amplify Count: {matchJson.AmplifyCount}";
+            childOfCell(8).GetChild(3).GetComponent<Text>().text = $"Team At Amp: {matchJson.TeamAtAmp}";
+            childOfCell(8).GetChild(4).GetComponent<Text>().text = $"Bots in Harmony: {matchJson.Harmony}";
+            childOfCell(8).GetChild(5).gameObject.SetActive(matchJson.Coopertition);
+            childOfCell(8).GetChild(6).GetComponent<Text>().text = $"Fouls: {matchJson.Fouls}";
+            childOfCell(9).GetChild(0).GetChild(0).GetChild(0).GetChild(0).GetComponent<Text>().text = $"Ranking Explanation: {matchJson.RankingComments}\nGeneral Strategy: {matchJson.StratComments}\nOther Comments:{matchJson.OtherComments}";
+            childOfCell(10).GetComponent<Text>().text = matchJson.WinMatch ? "WIN" : "LOSS";
             newMatchCell = Instantiate(newMatchCell, transform.parent);
 
 
 
 
         }
+    }
+
+    Transform childOfCell(int i) {
+        return matchPrefab.transform.GetChild(i);
     }
 }
